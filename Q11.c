32b029a107eb8118e7f05b4740031ab3dda9a54b
Arpita <Arghya.38854@stu.upes.ac.in>
2026-08-21 15:55:42 +0000

    question 11

---
+++ Q11.c
@@ -0,0 +1,16 @@
+//to check if an integer is odd or even
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter a number : ");
+scanf("%d", &a);
+if 
+((a / 2) * 2 == a) {
+printf("%d is Even \n", a);
+}
+else {
+printf("%d is odd \n", a);
+}
+return 0;
+}
